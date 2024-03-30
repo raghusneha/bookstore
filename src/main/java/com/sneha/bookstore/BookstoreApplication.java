dda4f8f8368e606159fc24eb7ab52824736c4ffa
narendra <narendrakareli@yahoo.com>
2024-03-30 17:35:26 +0530

    added profiling

--- src/main/java/com/sneha/bookstore/BookstoreApplication.java
+++ src/main/java/com/sneha/bookstore/BookstoreApplication.java
@@ -1,5 +1,8 @@
 package com.sneha.bookstore;
 
+import com.sneha.bookstore.profile.Profile;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -7,9 +10,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories
 public class BookstoreApplication {
+	@Autowired
+	private Profile messageComponent;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
-
+	@PostConstruct
+	public void init() {
+		String message = messageComponent.getMessage();
+		System.out.println("Message: " + message);
+	}
 }
